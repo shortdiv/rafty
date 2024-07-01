40a3ef333d57cada9e798cefb035549b0cff53d6
divya <hello@shortdiv.com>
2024-07-01 19:56:43 -0400

    feat: theres a diff between handle cast and handle call, call would timeout for counting for some reason

--- lib/rafty/node.ex
+++ lib/rafty/node.ex
@@ -14,7 +14,11 @@ defmodule Rafty.Node do
   end
 
   def request_vote(pid, candidate_term, candidate_id) do
-    GenServer.call(pid, {:request_vote, candidate_term, candidate_id})
+    GenServer.call(pid, {:request_vote, self(), candidate_term, candidate_id})
+  end
+
+  def vote_received(pid, somedata) do
+    GenServer.cast(pid, {:vote_received, somedata})
   end
 
   def get_state(pid) do
@@ -30,6 +34,7 @@ defmodule Rafty.Node do
       election_timer: nil,
       current_term: 0,
       voted_for: nil,
+      votes_received: 0,
       node_id: "node_#{initial_state[:id]}",
     }
     {:ok, st}
@@ -43,7 +48,6 @@ defmodule Rafty.Node do
   def crash(name), do: GenServer.cast(via_tuple(name), :raise)
 
   def handle_info({:timeout, _timer_ref, :election_timeout}, state) do
-    IO.inspect("#{state.node_id} has voted for #{state.voted_for}")
     if state.voted_for == nil do
       IO.inspect("becoming a candidate")
       Rafty.RegistryUtils.get_other_nodes(state.node_id)
@@ -51,8 +55,7 @@ defmodule Rafty.Node do
         process = Rafty.RegistryUtils.find_node_process(node)
         # increase the term
         state = Map.put(state, :current_term, state.current_term + 1)
-        reply = request_vote(process, state.current_term, state.node_id)
-        IO.inspect(reply)
+        request_vote(process, state.current_term, state.node_id)
         end)
       {:noreply, %{state | role: @candidate_role }}
     else
@@ -69,19 +72,46 @@ defmodule Rafty.Node do
   end
 
   @impl true
-  def handle_call({:request_vote, candidate_term, candidate_id}, _from, state) do
+  def handle_call({:request_vote, pid, candidate_term, candidate_id}, _from, state) do
+    IO.inspect(pid)
+    IO.inspect(candidate_id)
     {reply, new_state} =
       if candidate_term > state.current_term and state.voted_for == nil do
+        IO.inspect("#{state.node_id} currently voting for #{candidate_id}")
         {:ok, %{state | voted_for: candidate_id, current_term: candidate_term}}
       else
         {:error, state}
       end
 
+    if reply == :ok do
+      IO.inspect("counting vote")
+      vote_received(pid, %{})
+    end
+
     {:reply, reply, new_state}
+  end
+
+  def handle_call({:vote_received, _data}, _from, state) do
+    IO.inspect("received vote")
+    {:reply, :ok, state}
   end
 
   def handle_call(:get_state, _from, state) do
     {:reply, state, state}
+  end
+
+  @impl true
+  def handle_cast({:vote_received, _data}, state) do
+    votes_received = state.votes_received + 1
+    new_state = %{state | votes_received: votes_received}
+    role = if votes_received > div(length(Rafty.RegistryUtils.get_other_nodes(state.node_id)), 2) do
+      IO.inspect("a leader has been chosen #{state.node_id}")
+      :leader
+    else
+      :follower
+    end
+
+    {:noreply, %{new_state | role: role, election_timer: nil}}
   end
 
   defp via_tuple(id) do
