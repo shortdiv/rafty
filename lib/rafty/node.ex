60bf76b2b7e39edeef725f4bfe96f188cfac9d44
divya <hello@shortdiv.com>
2024-07-01 16:14:42 -0400

    feat: build initial voting logic so a node can't vote again after it already voted

--- lib/rafty/node.ex
+++ lib/rafty/node.ex
@@ -6,10 +6,19 @@ defmodule Rafty.Node do
   @heartbeat_interval 5000
   @election_timeout 4000
   @registry :node_registry
+  @candidate_role :candidate
 
   def start_link(opts \\ []) do
     id = Keyword.get(opts, :id)
     GenServer.start_link(__MODULE__, opts, name: via_tuple(id))
+  end
+
+  def request_vote(pid, candidate_term, candidate_id) do
+    GenServer.call(pid, {:request_vote, candidate_term, candidate_id})
+  end
+
+  def get_state(pid) do
+    GenServer.call(pid, :get_state)
   end
 
   @impl true
@@ -19,6 +28,8 @@ defmodule Rafty.Node do
       role: initial_state[:role],
       heartbeat_timer: :erlang.start_timer(randomize_timeout(@heartbeat_interval, 0.4), self(), :heartbeat_timeout),
       election_timer: nil,
+      current_term: 0,
+      voted_for: nil,
       node_id: "node_#{initial_state[:id]}",
     }
     {:ok, st}
@@ -31,27 +42,39 @@ defmodule Rafty.Node do
 
   def crash(name), do: GenServer.cast(via_tuple(name), :raise)
 
-  def handle_info({:timeout, _timer_ref, :election_timeout}, st) do
-    IO.puts("starting an election! #{st.node_id}")
-    Rafty.RegistryUtils.get_other_nodes(st.node_id)
+  def handle_info({:timeout, _timer_ref, :election_timeout}, state) do
+    IO.puts("starting an election! #{state.node_id}")
+    Rafty.RegistryUtils.get_other_nodes(state.node_id)
     |> Enum.each(fn node ->
       process = Rafty.RegistryUtils.find_node_process(node)
-      send(process, {:message, "Hello from #{st.node_id}"})
+      # increase the term
+      state = Map.put(state, :current_term, state.current_term + 1)
+      request_vote(process, state.current_term, state.node_id)
       end)
-    {:noreply, st}
+    {:noreply, %{state | role: @candidate_role }}
   end
 
   @impl true
   def handle_info({:timeout, _timer_ref, :heartbeat_timeout}, st) do
     IO.puts("heartbeat exceeded")
-    # start an election!
     :erlang.cancel_timer(st.heartbeat_timer)
     {:noreply, %{st | election_timer: :erlang.start_timer(randomize_timeout(@election_timeout, 0.4), self(), :election_timeout)}}
   end
 
-  def handle_info({:message, message}, state) do
-    IO.inspect("#{message} received by #{state.node_id}")
-    {:noreply, state}
+  @impl true
+  def handle_call({:request_vote, candidate_term, candidate_id}, _from, state) do
+    {reply, new_state} =
+      if candidate_term > state.current_term and state.voted_for == nil do
+        {:ok, %{state | voted_for: candidate_id, current_term: candidate_term}}
+      else
+        {:error, state}
+      end
+
+    {:reply, reply, new_state}
+  end
+
+  def handle_call(:get_state, _from, state) do
+    {:reply, state, state}
   end
 
   defp via_tuple(id) do
